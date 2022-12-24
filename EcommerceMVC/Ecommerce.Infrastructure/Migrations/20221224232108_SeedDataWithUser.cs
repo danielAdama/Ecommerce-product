680ee42f9d849d265a0e4c02e16ea85c01c7bf5f
danielAdama <adamadaniel321@gmail.com>
2022-12-25 00:22:56 +0100

    Working on Company Controllrt

---
+++ EcommerceMVC/Ecommerce.Infrastructure/Migrations/20221224232108_SeedDataWithUser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Migrations
+{
+    public partial class SeedDataWithUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    StreetAddress = table.Column<string>(type: "text", nullable: true),
+                    City = table.Column<string>(type: "text", nullable: true),
+                    State = table.Column<string>(type: "text", nullable: true),
+                    PostalCode = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true),
+                    TimeCreated = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    TimeUpdated = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1L,
+                columns: new[] { "TimeCreated", "TimeUpdated" },
+                values: new object[] { new DateTimeOffset(new DateTime(2022, 12, 24, 23, 21, 8, 732, DateTimeKind.Unspecified).AddTicks(5071), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2022, 12, 24, 23, 21, 8, 732, DateTimeKind.Unspecified).AddTicks(5071), new TimeSpan(0, 0, 0, 0, 0)) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1L,
+                columns: new[] { "TimeCreated", "TimeUpdated" },
+                values: new object[] { new DateTimeOffset(new DateTime(2022, 12, 22, 23, 27, 14, 42, DateTimeKind.Unspecified).AddTicks(2829), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2022, 12, 22, 23, 27, 14, 42, DateTimeKind.Unspecified).AddTicks(2828), new TimeSpan(0, 0, 0, 0, 0)) });
+        }
+    }
+}
--- EcommerceMVC/EcommerceMVC/Areas/Admin/Controllers/CompanyController.cs
+++ EcommerceMVC/EcommerceMVC/Areas/Admin/Controllers/CompanyController.cs
@@ -1,12 +1,97 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ecommerce.Infrastructure.Data;
+using Ecommerce.Infrastructure.Data.DTO;
+using Ecommerce.Infrastructure.Services.Interface;
+using EcommerceMVC.Services.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceMVC.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CompanyController : Controller
     {
-        public IActionResult Index()
+        private readonly EcommerceDbContext _context;
+
+        public CompanyController(EcommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        public ViewResult Index()
         {
             return View();
         }
+
+        public async Task<IActionResult> Upsert(long id)
+        {
+            Company company = new();
+
+
+            if (id is not 0)
+            {
+                return View(company);
+
+            }
+            var getCompany = await _context.Companies.FirstOrDefaultAsync(x => x.Id == id);
+            return View(getCompany);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upsert(Company company, CancellationToken cancellationToken)
+        {
+            if (ModelState.IsValid)
+            {
+                using var transaction = await _context.Database.BeginTransactionAsync(cancellationToken);
+                try
+                {
+                    if (company.Id == 0)
+                    {
+                        await _context.Companies.AddAsync(company, cancellationToken);
+                    }
+                    if (company.Id != 0)
+                    {
+                        _context.Companies.Update(company);
+                        TempData["success"] = "Company updated successfully";
+                    }
+                    await _context.SaveChangesAsync(cancellationToken);
+                    await transaction.CommitAsync(cancellationToken);
+                    TempData["success"] = "Company created successfully";
+                }
+                catch (Exception ex)
+                {
+                    await transaction.RollbackAsync(cancellationToken);
+                    TempData["errorMessage"] = ex.Message;
+                    return RedirectToAction("Index");
+                }
+
+            }
+            return View(company);
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var companyList = await _context.Companies.AsNoTracking().ToListAsync();
+            return Json(new { data = companyList });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(long id)
+        {
+            var company = await _context.Companies.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (company == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _context.Companies.Remove(company);
+            await _context.SaveChangesAsync();
+            return Json(new { success = true, message = "Deleting Successful" });
+        }
+        #endregion
+
     }
 }
+
