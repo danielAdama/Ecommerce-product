ca354d82bba6bb8c0136bf3fe166035520b50b57
danielAdama <adamadaniel321@gmail.com>
2022-12-28 18:40:17 +0100

    Complete Order.js

--- EcommerceMVC/EcommerceMVC/wwwroot/js/product.js
+++ EcommerceMVC/EcommerceMVC/wwwroot/js/product.js
@@ -4,33 +4,38 @@ $(document).ready(function () {
     loadDataTable()
 });
 
+
 function loadDataTable() {
-    dataTable = $('#tblData').DataTable({
-            'ajax': {
-                'url': 'Product/GetAll'
-            },
-            'columns': [
-                { "data": "name", "width": "15%" },
-                { "data": "description", "width": "15%" },
-                { "data": "price", "width": "15%" },
-                { "data": "category.name", "width": "15%" },
-                {
-                    "data": "id",
-                    "render": function (data) {
-                        return `
+    fetch("https://localhost:44392/Admin/Product/GetAll")
+        .then(resp => resp.json())
+        .then(data => {
+            $('#tblData').DataTable({
+                data: data,
+                column: [
+                    { "data": "name", "width": "15%" },
+                    { "data": "description", "width": "15%" },
+                    { "data": "price", "width": "15%" },
+                    { "data": "category.name", "width": "15%" },
+                    {
+                        "data": "id",
+                        "render": function (data) {
+                            return `
                                 <div class="w-75 btn-group" role="group">
-                                    <a href="/Admin/Product/Upsert?id=${data}" 
+                                    <a href="/Admin/Product/Upsert?id=${data}"
                                     class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit</a>
-                                    <a onclick=Delete('/Admin/Product/Delete/${data}') 
+                                    <a onclick=Delete('/Admin/Product/Delete/${data}')
                                     class="btn btn-danger mx-2"><i class="bi bi-trash3-fill"></i> Delete</a>
                                 </div>
                                 `
+                        },
+                        "width": "15%"
                     },
-                    "width": "15%"
-                },
-            ]
-        });
-}
+                ],
+            })
+        })
+};
+
+
 
 function Delete(url) {
     Swal.fire({
